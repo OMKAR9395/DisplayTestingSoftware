3c24c0fd59f5fd1691d5d3513882e004c0898c58
OMKAR9395 <omkarmohite85@gmail.com>
2025-12-31 14:28:27 +0530

    server code

--- src/services/printer.service.ts
+++ src/services/printer.service.ts
@@ -6,25 +6,16 @@ import { environment } from '../environment/environment.dev';
 
 @Injectable({ providedIn: 'root' })
 export class PrinterService {
-
-  private api = environment.apiUrl;
+  // private api = environment.apiUrl;
+  private api = environment.apiUrl + '/send';
 
   constructor(private http: HttpClient) {}
 
   sendCommand(command: string) {
-    return this.http.post<any>(this.api, { command })
-      .pipe(
-        catchError((error: HttpErrorResponse) => {
-          let msg = 'Unknown error';
-
-          if (error.error?.message) {
-            msg = error.error.message;
-          } else if (error.status === 0) {
-            msg = 'Node server not running';
-          }
-
-          return throwError(() => msg);
-        })
-      );
+    return this.http.post<any>(this.api, { text: command }).pipe(
+      catchError((error: HttpErrorResponse) => {
+        return throwError(() => error.error?.message || 'Server not reachable');
+      })
+    );
   }
 }
